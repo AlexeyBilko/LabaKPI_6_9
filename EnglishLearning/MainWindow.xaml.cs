f6f40ddbf646821ad8f74f3a5ee0390a4e8fd724
Alexey Bilko <bilkoalexey67@gmail.com>
2021-05-19 14:44:59 +0300

    created log in, classe user

--- EnglishLearning/MainWindow.xaml.cs
+++ EnglishLearning/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,48 @@ namespace EnglishLearning
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<User> users;
+
         public MainWindow()
         {
+            users = new List<User>();
+            GetUsers();
+
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in users)
+            {
+                if (LoginTextBox.Text == item.Login && passwordBox.Password == item.Password)
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private void GetUsers()
+        {
+            try
+            {
+                List<string> list = File.ReadAllLines("users.txt").ToList();
+                foreach (var item in list)
+                {
+                    string[] tmp = item.Split(' ');
+                    users.Add(new User(tmp[0], tmp[1], int.Parse(tmp[2]), int.Parse(tmp[3])));
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error in file!");
+                Environment.Exit(0);
+            }
         }
     }
 }
