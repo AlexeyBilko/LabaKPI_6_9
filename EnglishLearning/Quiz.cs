fddb3d58f80984d68f561ac98db1f9e25a4f1fbb
Dmytro Hushchin <dhuchchin@gmail.com>
2021-05-19 15:09:43 +0300

    Quiz class

--- EnglishLearning/Quiz.cs
+++ EnglishLearning/Quiz.cs
@@ -8,5 +8,20 @@ namespace EnglishLearning
 {
     class Quiz
     {
+        string Question;
+        public List<String> Tests;
+        int Answer;
+        public Quiz(string question, int answer, string test1, string test2, string test3, string test4)
+        {
+            Tests = new List<string>();
+            Question = question;
+            Tests.Add(test1);
+            Tests.Add(test2);
+            Tests.Add(test3);
+            Tests.Add(test4);
+            Answer = answer;
+
+        }
+
     }
 }
